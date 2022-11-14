aeda6f0f3e5ddcc26f812a912e2aaa6351be2a28
chisna2 <marksman703@gmail.com>
2022-11-14 10:53:21 +0900

    shell 문법 ps1 문법으로 컨버젼 1

--- 3-docker-kubernetes/2-docker-volume/docker-volume.ps1
+++ 3-docker-kubernetes/2-docker-volume/docker-volume.ps1
@@ -4,11 +4,11 @@ docker volume create --name db
 
 docker volume ls
 
-docker run \
-  -d \
-  --name fastcampus-mysql \
-  -e MYSQL_DATABASE=fastcampus \
-  -e MYSQL_ROOT_PASSWORD=fastcampus \
-  -v db:/var/lib/mysql \
-  -p 3306:3306 \
+docker run `
+  -d `
+  --name fastcampus-mysql `
+  -e MYSQL_DATABASE=fastcampus `
+  -e MYSQL_ROOT_PASSWORD=fastcampus `
+  -v db:/var/lib/mysql `
+  -p 3306:3306 `
   mysql:5.7
